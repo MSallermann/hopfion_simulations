0632052daf6ca05926a250db3e5d433e7434f333
Moritz <m.sallermann@fz-juelich.de>
2021-10-07 13:28:08 +0200

    hopfion_path: can now also take an initial chain as input

--- hopfion_path.py
+++ hopfion_path.py
@@ -7,8 +7,9 @@ parser.add_argument('-o',  dest="output_folder", type=str, nargs='?', default="o
 parser.add_argument('-p',  dest="plot_folder", type=str, nargs='?', default="plots", help='The plot folder, relative to output folder')
 parser.add_argument('-t',  dest="output_tag", type=str, nargs='?', default="<time>", help='The output file tag')
 parser.add_argument('-f',  dest="input_file", type=str, nargs='?', default="input.cfg", help='The input file')
-parser.add_argument('-ii', dest="initial_image", required=True, type=str, nargs='?', help='The initial image')
-parser.add_argument('-if', dest="final_image",   required=True, type=str, nargs='?', help='The final image')
+parser.add_argument('-ii', dest="initial_image", required=False, type=str, nargs='?', help='The initial image')
+parser.add_argument('-if', dest="final_image",   required=False, type=str, nargs='?', help='The final image')
+parser.add_argument('-ic', dest="initial_chain",   required=False, type=str, nargs='?', help='The initial chain')
 parser.add_argument('--dry', dest="dry_run", action="store_true")
 
 from spirit_python_utilities.spirit_utils import import_spirit, util, plotting, Spin_System
@@ -38,33 +39,20 @@ def main():
 
     with state.State(args.input_file) as p_state:
         util.set_output_folder(p_state, args.output_folder, tag=args.output_tag)
-
-        # n_cells = geometry.get_n_cells(p_state)
-        # configuration.plus_z(p_state)
-        # configuration.hopfion(p_state, radius=3)
-        # # configuration.skyrmion(p_state, 5)
-
-        # io.image_write(p_state, os.path.join(args.output_folder, "initial_hopfion.ovf"))
-        # simulation.start(p_state, simulation.METHOD_LLG, simulation.SOLVER_LBFGS_OSO)
-        # io.image_write(p_state, os.path.join(args.output_folder, "final_hopfion.ovf"))
-
         import matplotlib.pyplot as plt
-        # spin_system = Spin_System.spin_system_from_p_state(p_state)
-        # import math
 
-        # c_val = int(n_cells[2]/2)
-        # print(c_val)
-        # plotting.plot_spins_2d(spin_system.c_slice( 0 ), plt.gca(), scale=0.8)
-        # plt.savefig(plot_folder + "/cut_xy_z_{}.png".format(c_val), dpi=300)
-        # plt.close()
-
-        noi = 20
-        chain.set_length(p_state, noi)
-
-        io.image_read(p_state, args.initial_image, idx_image_inchain=0)
-        io.image_read(p_state, args.final_image, idx_image_inchain=noi-1)
-
-        transition.homogeneous(p_state, 0, noi-1)
+        noi = -1
+        if args.initial_image and args.final_image:
+            noi = 10
+            chain.set_length(p_state, noi)
+            io.image_read(p_state, args.initial_image, idx_image_inchain=0)
+            io.image_read(p_state, args.final_image, idx_image_inchain=noi-1)
+            transition.homogeneous(p_state, 0, noi-1)
+        elif args.initial_chain:
+            io.chain_read(p_state, args.initial_chain)
+            noi = chain.get_noi(p_state)
+        else:
+            raise Exception("No initial chain or initial and final image supplied")
 
         energy_path = plotting.energy_path_from_p_state(p_state)
         print(energy_path.total_energy)
