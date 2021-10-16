afa458793012aa87e53a760a5b6b766157a48b61
Moritz Sallermann <m.sallermann@fz-juelich.de>
2021-10-16 11:18:27 +0000

    Added script gneb_workflow_test.py that uses the new gneb workflow

---
+++ gneb_workflow_test.py
@@ -0,0 +1,66 @@
+import argparse
+
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o',  dest="output_folder", type=str, nargs='?', default="output", help='The output folder')
+parser.add_argument('-t',  dest="output_tag", type=str, nargs='?', default="<time>", help='The output file tag')
+parser.add_argument('-f',  dest="input_file", type=str, nargs='?', default="input.cfg", help='The input file')
+parser.add_argument('-ic', dest="initial_chain",   required=False, type=str, nargs='?', help='The initial chain')
+parser.add_argument('--dry', dest="dry_run", action="store_true")
+
+from spirit_python_utilities.spirit_utils import import_spirit, util, plotting, data, gneb_workflow
+
+def main():
+
+    import os
+    args = parser.parse_args()
+
+    if args.dry_run:
+        print("performing dry run!")
+
+    output_folder = args.output_folder
+    if not os.path.exists(args.output_folder):
+        os.makedirs(args.output_folder)
+
+    print("Saving output to:", args.output_folder)
+    spirit_info = import_spirit.find_and_insert("~/Coding", stop_on_first_viable=True, choose = lambda x: (not x.cuda) and (x.openMP) )[0]
+    print(spirit_info)
+
+    from spirit import state, configuration, simulation, io, geometry, chain, transition, hamiltonian
+    from spirit.parameters import gneb
+    import os
+
+    gnw = gneb_workflow.GNEB_Node(args.output_folder, initial_chain_file=args.initial_chain, input_file=args.input_file, output_folder=args.output_folder)
+
+    gnw.n_iterations_check = 500
+
+    def state_prepare_cb(gnw, p_state):
+        hamiltonian.set_anisotropy(p_state, 1e-3, [0,0,1])
+
+    def before_gneb_cb(gnw, p_state):
+        simulation.start(p_state, simulation.METHOD_GNEB, simulation.SOLVER_VP, n_iterations=1)
+        gnw.current_energy_path = data.energy_path_from_p_state(p_state)
+        plotting.plot_energy_path(gnw.current_energy_path, plt.gca())
+        plt.savefig(gnw.output_folder + "/path_{}_initial.png".format(gnw.total_iterations))
+        plt.close()
+
+    def step_cb(gnw, p_state):
+        plotting.plot_energy_path(gnw.current_energy_path, plt.gca())
+        plt.savefig(gnw.output_folder + "/path_{}.png".format(gnw.total_iterations))
+        plt.close()
+
+    def exit_cb(gnw, p_state):
+        plotting.plot_energy_path(gnw.current_energy_path, plt.gca())
+        plt.savefig(gnw.output_folder + "/path_{}_final.png".format(gnw.total_iterations))
+        plt.close()
+
+    gnw.state_prepare_callback = state_prepare_cb
+    gnw.before_gneb_callback   = before_gneb_cb
+    gnw.gneb_step_callback     = step_cb
+    gnw.exit_callback          = exit_cb
+
+    gnw.run()
+
+if __name__ == "__main__":
+    main()
