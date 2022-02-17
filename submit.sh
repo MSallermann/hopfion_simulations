479143d761dd4b319bad6c0abc886bf112a80beb
Moritz Sallermann <M.Sallermann@fz-juelich.de>
2022-02-17 10:55:25 +0100

    Delete all unnecessary files

--- submit.sh
+++
@@ -1,4 +0,0 @@
-export OMP_NUM_THREADS=4
-nohup python3 hopfion_path.py -ii input_images/final_hopfion.ovf          -if input_images/intermediate_minimum_1.ovf -o second_im1 &
-nohup python3 hopfion_path.py -ii input_images/intermediate_minimum_1.ovf -if input_images/intermediate_minimum_2.ovf -o second_im2 &
-nohup python3 hopfion_path.py -ii input_images/intermediate_minimum_2.ovf -if input_images/ferromagnet.ovf            -o second_ferr &
