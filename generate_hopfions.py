c74084d3badbc6993f5d1b57a0c9721edd3be2e2
Moritz <m.sallermann@fz-juelich.de>
2022-02-21 15:09:31 +0100

    generate_hopfions: improvements

--- generate_hopfions.py
+++ generate_hopfions.py
@@ -15,12 +15,13 @@ OUTPUT_BASE_FOLDER = os.path.join(SCRIPT_DIR, "gamma_l0_calculations")
 INPUT_FILE         = SCRIPT_DIR + "/input.cfg"
 
 def main():
+    gamma_list = [i/7.0 for i in range(8)]
+    gamma_list = [1.0]
+    l0_list    = [2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
 
-    for i in range(8):
-        gamma = float(i)/7.0
-        gamma = 6.0/7.0
-        # l0    = 2.5 + float(i)/8.0 # A
-        l0 = 5.0
+    gamma_l0_list = [[gamma, l0] for gamma in gamma_list for l0 in l0_list]
+
+    for gamma, l0 in gamma_l0_list:
 
         name = "gamma_{:.3f}_l0_{:.3f}".format(gamma, l0)
 
@@ -48,7 +49,7 @@ def main():
             configuration.domain(p_state, [0,0,1])
             hamiltonian.set_exchange(p_state, len(J), J)
 
-        initial_path_creator.main(output_file = folder.get_initial_chain_file_path(), input_file = INPUT_FILE, noi=10, background=[0,0,1], radius=3, hopfion_normal=[0,0,1], state_prepare_callback = __state_prepare_cb, shrinking_hopfion = False)
+        initial_path_creator.main(output_file = folder.get_initial_chain_file_path(), input_file = INPUT_FILE, noi=10, background=[0,0,1], radius=5, hopfion_normal=[0,0,1], state_prepare_callback = __state_prepare_cb, shrinking_hopfion = False)
 
 if __name__ == "__main__":
     main()
