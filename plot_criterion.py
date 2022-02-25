0b27855ca28f72b3c86509d5c1c48833d2da0ce5
Moritz <m.sallermann@fz-juelich.de>
2022-02-25 19:30:00 +0100

    plot_criterion: can now plot grid of calculations vs nikolai criterion

--- plot_criterion.py
+++ plot_criterion.py
@@ -1,23 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from urllib3 import Retry
 
-d = 1 #AA
-l0 = 2 #AA
+pastell_red   = np.array( [255, 105, 97, 255] ) / 255
+pastell_green = np.array( [119, 221, 119, 255] ) / 255
 
-gamma_list = np.linspace(0,1)
+def main(data):
+    data = np.asarray(data)
+    # print(data)
 
-def lhs(gamma):
-    return np.array([ max(g, 6.0 * (1.0 - g)) for g in gamma])
+    d  = 1 #AA
+    l0 = 3.5 #AA
 
-def rhs(d, l0):
-    return 6.5 * (d/l0)**2
+    gamma_list = np.linspace(0,1)
 
-plt.plot( gamma_list, lhs(gamma_list), color="r" )
-plt.axhline( rhs(d, l0), color="b" )
-plt.xlabel(r"$\gamma$")
-plt.savefig("stability_criterion.png", dpi=300)
+    def lhs(gamma):
+        return np.array([ max(g, 6.0 * (1.0 - g)) for g in gamma])
 
+    def rhs(d, l0):
+        return 6.5 * (d/l0)**2
 
-l0_list    = np.linspace(0,1)
-plt.contourf( gamma_list, l0_list, lhs(gamma_list) - rhs(d,l0_list) )
-plt.savefig("stability_criterion2.png", dpi=300)
+    def r0_min(gamma, d):
+        return np.sqrt( 6.5 * d**2 / (max(gamma, 6.0 * (1.0 - gamma))) )
+
+    # plt.axhline( rhs(d, l0), color="b" )
+    # plt.xlabel(r"$\gamma$")
+    # plt.savefig("stability_criterion.png", dpi=300)
+
+    # l0_list    = np.linspace(0,1)
+    # plt.contourf( gamma_list, l0_list, lhs(gamma_list) - rhs(d,l0_list) )
+
+    l0_approx = 0.5 * l0 * np.sqrt( np.array([ max(g, 6.0 * (1.0 - g)) for g in gamma_list]) )
+
+    # plt.plot( gamma_list, l0_approx, color="r" )
+
+    mask_greater_than_zero = data[:,2] > 1e-5
+    plt.fill_between( gamma_list, 0, [r0_min(g, d) for g in gamma_list], color = pastell_red, alpha=0.3 )
+    plt.fill_between( gamma_list, [r0_min(g, d) for g in gamma_list], 5, color = pastell_green, alpha=0.3 )
+
+    plt.plot( data[mask_greater_than_zero,0], data[mask_greater_than_zero,1],   marker="o", markersize = 5, markeredgewidth = 1, mec = "black", markerfacecolor=pastell_green, ls="None" )
+    plt.plot( data[~mask_greater_than_zero,0], data[~mask_greater_than_zero,1], marker="o", markersize = 5, markeredgewidth = 1, mec = "black", markerfacecolor=pastell_red, ls="None" )
+    plt.xlabel(r"$\gamma$")
+    plt.ylabel(r"$r_0~[a]$")
+    plt.savefig("stability_criterion.png", dpi=300, bbox_inches="tight")
+    plt.show()
+
+if __name__ == "__main__":
+    import calculation_folder
+    import glob
+    import argparse, os
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', dest="input_folder", type=str, nargs='?', help='The input folder')
+
+    args = parser.parse_args()
+
+    files = glob.glob( args.input_folder)
+
+    data  = [] # angle > 0
+    for f in files:
+        calculation = calculation_folder.calculation_folder(f)
+        data.append( [ calculation.descriptor["gamma"], calculation.descriptor["l0"], calculation.descriptor["max_angle_between_neighbours"]] )
+
+    main(data)
