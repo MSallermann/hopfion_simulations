65978f1be28a04ce32cdc1e439cad629d3b8a5c8
Moritz <m.sallermann@fz-juelich.de>
2022-03-02 15:58:00 +0100

    plot_criterion: made it nicer

--- plot_criterion.py
+++ plot_criterion.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+
+#define font family to use for all text
 from urllib3 import Retry
 
-pastell_red   = np.array( [255, 105, 97, 255] ) / 255
-pastell_green = np.array( [119, 221, 119, 255] ) / 255
+matplotlib.rcParams["mathtext.fontset"] = "dejavuserif" #'dejavusans' (default),
+                               # 'dejavuserif', 'cm' (Computer Modern), 'stix',
+                               # 'stixsans'
 
 def main(data):
     data = np.asarray(data)
@@ -14,18 +18,8 @@ def main(data):
 
     gamma_list = np.linspace(0,1)
 
-    def lhs(gamma):
-        return np.array([ max(g, 6.0 * (1.0 - g)) for g in gamma])
-
-    def rhs(d, l0):
-        return 6.5 * (d/l0)**2
-
     def r0_min(gamma, d):
         return np.sqrt( 6.5 * d**2 / (max(gamma, 6.0 * (1.0 - gamma))) )
-
-    # plt.axhline( rhs(d, l0), color="b" )
-    # plt.xlabel(r"$\gamma$")
-    # plt.savefig("stability_criterion.png", dpi=300)
 
     # l0_list    = np.linspace(0,1)
     # plt.contourf( gamma_list, l0_list, lhs(gamma_list) - rhs(d,l0_list) )
@@ -35,11 +29,11 @@ def main(data):
     # plt.plot( gamma_list, l0_approx, color="r" )
 
     mask_greater_than_zero = data[:,2] > 1e-5
-    plt.fill_between( gamma_list, 0, [r0_min(g, d) for g in gamma_list], color = pastell_red, alpha=0.3 )
-    plt.fill_between( gamma_list, [r0_min(g, d) for g in gamma_list], 5, color = pastell_green, alpha=0.3 )
+    plt.fill_between( gamma_list, [r0_min(g, d) for g in gamma_list], 5, color = "royalblue", alpha=0.45 )
+    plt.fill_between( gamma_list, 0, [r0_min(g, d) for g in gamma_list], color = "lightsalmon", alpha=0.45 )
 
-    plt.plot( data[mask_greater_than_zero,0], data[mask_greater_than_zero,1],   marker="o", markersize = 5, markeredgewidth = 1, mec = "black", markerfacecolor=pastell_green, ls="None" )
-    plt.plot( data[~mask_greater_than_zero,0], data[~mask_greater_than_zero,1], marker="o", markersize = 5, markeredgewidth = 1, mec = "black", markerfacecolor=pastell_red, ls="None" )
+    plt.plot( data[mask_greater_than_zero,0], data[mask_greater_than_zero,1],   marker="o", markersize = 6, markeredgewidth = 1.0, mec = "black", markerfacecolor="royalblue", ls="None" )
+    plt.plot( data[~mask_greater_than_zero,0], data[~mask_greater_than_zero,1], marker="X", markersize = 7, markeredgewidth = 1.0, mec = "black", markerfacecolor="lightsalmon", ls="None" )
     plt.xlabel(r"$\gamma$")
     plt.ylabel(r"$r_0~[a]$")
     plt.savefig("stability_criterion.png", dpi=300, bbox_inches="tight")
