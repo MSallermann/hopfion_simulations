a3ea1d47e9d8d0dbb792694776c1a1fd044b9437
Moritz <m.sallermann@fz-juelich.de>
2022-03-05 19:40:24 +0100

    gneb_preconverge: handling of input/output paths should now be much better

--- preconverge_paths.py
+++ preconverge_paths.py
@@ -7,36 +7,47 @@ import calculation_folder
 SCRIPT_DIR = os.path.dirname( os.path.abspath(__file__) )
 INPUT_FILE = SCRIPT_DIR + "/input.cfg"
 
-def main(input_path, output_path=None):
+def main(input_calculation_folder_path, output_calculation_folder_path=None):
 
-    calculation = calculation_folder.calculation_folder(input_path)
+    # Read calculation folder from input path
+    calculation = calculation_folder.calculation_folder(input_calculation_folder_path)
 
-    if not output_path:
-        output_path = os.path.join(calculation.output_folder, "gneb_preconverge")
+    # If no output calculation folder is give we assume it is the same as the input
+    if output_calculation_folder_path is None:
+        output_calculation_folder_path = input_calculation_folder_path
 
+    if input_calculation_folder_path != output_calculation_folder_path:
+        # Create new calculation folder at output path
+        calculation_out = calculation_folder.calculation_folder(output_calculation_folder_path)
+        calculation_out.descriptor = calculation.descriptor.copy()
+        calculation_out.to_json()
+    else:
+        calculation_out = calculation
+
+    output_path_gneb = os.path.join(calculation_out.output_folder, "gneb_preconverge_test")
+
+    if os.path.exists(output_path_gneb):
+        print(f"Skipping because {output_path_gneb} already exists")
+        return
+
+    # Where in the input calculation folder is the initial chain file saved?
     initial_chain_file = calculation.to_abspath(calculation.descriptor["initial_chain_file"])
     initial_chain_file = calculation.to_abspath(os.path.join("gneb_preconverge", "chain.ovf"))
 
-    # print(path)
-    # print("gamma = {}, l0 = {}".format(calculation.descriptor["gamma"], calculation.descriptor["l0"]))
-
-    # if os.path.exists( calculation.to_abspath("gneb_preconverge") ):
-    #     print("Skip")
-    #     return
-    # print("---")
-
+    # Write state prepare callback
     def state_prepare_cb(gnw, p_state):
         from spirit import geometry, configuration, hamiltonian
         geometry.set_n_cells(p_state, calculation.descriptor["n_cells"])
         configuration.domain(p_state, [0,0,1])
         hamiltonian.set_exchange(p_state, len(calculation.descriptor["J"]), calculation.descriptor["J"])
 
-    gnw = gneb_workflow.GNEB_Node(name="gneb_preconverge", input_file = INPUT_FILE, output_folder = output_path, initial_chain_file = initial_chain_file)
+    # Settings for GNEB workflow
+    gnw = gneb_workflow.GNEB_Node(name="gneb_preconverge", input_file = INPUT_FILE, output_folder = output_path_gneb, initial_chain_file = initial_chain_file)
     gnw.state_prepare_callback = state_prepare_cb
-    gnw.target_noi = 16
-    gnw.max_total_iterations = 100000
+    gnw.target_noi             = 16
+    gnw.max_total_iterations   = 100000
     gnw.setup_plot_callbacks()
-    gnw.convergence = 1e-5
+    gnw.convergence            = 1e-5
     gnw.to_json()
     gnw.run()
 
@@ -56,28 +67,29 @@ if __name__ == "__main__":
             main(f)
     else:
         OUTPUT_SCRATCH = "/SCRATCH/saller"
-        OUTPUT_LOCAL   = "/local/th1/iff003/saller/mpi_test"
+        OUTPUT_LOCAL   = "/local/th1/iff003/saller/"
+
+        ## for debugging:
+        # OUTPUT_SCRATCH = "./SCRATCH"
+        # OUTPUT_LOCAL   = "./LOCAL"
 
         from mpi_loop import mpi_loop
-        import shutil
+        import shutil, os
         from datetime import datetime
         now = datetime.now()
 
         def callable(i):
             input_path = args.paths[i]
-            calculation = calculation_folder.calculation_folder(input_path)
 
-            gamma = calculation.descriptor["gamma"]
-            l0    = calculation.descriptor["l0"]
+            calculation_name = os.path.basename(os.path.normpath(input_path))
+            # Identifier
+            identify = time = now.strftime("gneb_preconverge#%m_%d_%Y_%H_%M_%S")
 
-            identify = time = now.strftime("%m_%d_%Y_%H_%M_%S")
+            output_path = os.path.join(OUTPUT_SCRATCH, identify, calculation_name)
 
-            calculation_name = f"gamma_{gamma:.3f}_l0_{l0:.3f}#{identify}"
-
-            output_path = os.path.join(OUTPUT_SCRATCH, calculation_name, "gneb_preconverge2")
             main(input_path, output_path)
 
             print("Copy from SCRATCH to local")
-            shutil.copytree(os.path.join(OUTPUT_SCRATCH, calculation_name), os.path.join(OUTPUT_LOCAL, calculation_name))
+            shutil.copytree(os.path.join(OUTPUT_SCRATCH, identify, calculation_name), os.path.join(OUTPUT_LOCAL, identify, calculation_name))
 
         mpi_loop(callable, len(args.paths))
