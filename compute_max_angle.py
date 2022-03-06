b0f385efb164a4372618dcfe3b03ba21b5658c2c
Moritz <m.sallermann@fz-juelich.de>
2022-03-06 22:32:54 +0100

    compute_max_angle: fixed os import

--- compute_max_angle.py
+++ compute_max_angle.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import calculation_folder
 import json
+import os
 
 def main(path):
     from spirit import state, geometry, chain, simulation, io
