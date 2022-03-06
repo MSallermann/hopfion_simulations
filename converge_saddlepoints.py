aebd8c7dbff8573c9908dc19be76b59637ae50bf
Moritz <m.sallermann@fz-juelich.de>
2022-03-06 22:38:09 +0100

    converge_saddlepoints: tuned parameters after testing with 32^3 systems

--- converge_saddlepoints.py
+++ converge_saddlepoints.py
@@ -23,13 +23,13 @@ def main(input_calculation_folder_path, output_calculation_folder_path=None):
     else:
         calculation_out = calculation
 
-    output_path_gneb = os.path.join(calculation_out.output_folder, "gneb_sp")
+    output_path_gneb = os.path.join(calculation_out.output_folder, "gneb_sp_new")
 
     if os.path.exists(output_path_gneb):
         print(f"Skipping because {output_path_gneb} already exists")
         return
 
-    initial_chain_file = calculation.to_abspath(os.path.join("gneb_ci", "chain.ovf"))
+    initial_chain_file = calculation.to_abspath(os.path.join("gneb_preconverge", "chain.ovf"))
 
     # Write state prepare callback
     def state_prepare_cb(gnw, p_state):
@@ -46,22 +46,25 @@ def main(input_calculation_folder_path, output_calculation_folder_path=None):
     gnw.prepare_moving_endpoints()
     gnw.allow_split = False
     rx = gnw.current_energy_path.reaction_coordinate
-    gnw.delta_Rx_left    = (rx[1] - rx[0])
-    gnw.delta_Rx_right   = (rx[2] - rx[1])
+    gnw.delta_Rx_left    = 0.1 #(rx[1] - rx[0]) / 10
+    gnw.delta_Rx_right   = 0.1 #(rx[2] - rx[1]) / 10
 
     gnw.n_iterations_check   = 5000
-    gnw.max_total_iterations = 100000
-    gnw.convergence          = 1e-6
+    gnw.max_total_iterations = 1000000
+    gnw.convergence          = 1e-5
+    from spirit import simulation
+    gnw.solver_gneb = simulation.SOLVER_VP_OSO
     gnw.to_json()
 
     # Run with VP solver
+    gnw.log("Run with vp")
     gnw.run()
-
+    gnw.log("Run with lbfgs")
     # Run with LBFGS solver
-    # gnw.solver_gneb = simulation.SOLVER_LBFGS_OSO
-    # gnw.convergence = 1e-7
+    gnw.solver_gneb = simulation.SOLVER_LBFGS_OSO
+    gnw.convergence = 1e-6
     # gnw.max_total_iterations = 60000
-    # gnw.run()
+    gnw.run()
     # calculation.unlock()
 
 if __name__ == "__main__":
