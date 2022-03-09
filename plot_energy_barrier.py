27d7e20c6c12b00b1b608289f2cf6d9e9834ebd4
Moritz <m.sallermann@fz-juelich.de>
2022-03-09 16:48:26 +0100

    plot_energy_barrier: improvements

--- plot_energy_barrier.py
+++ plot_energy_barrier.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import gradient_line
 
 #define font family to use for all text
 from urllib3 import Retry
@@ -12,6 +13,7 @@ matplotlib.rcParams["mathtext.fontset"] = "dejavuserif" #'dejavusans' (default),
 def main(data):
     data = np.asarray(data)
 
+    # Plot vs r0
     for i in range(8):
         g = i/7.0
         tmp = data[ data[:,0] == g ]
@@ -22,15 +24,42 @@ def main(data):
         elif i==7:
             label_text = fr"$\gamma = 1$"
         else:
-            label_text = fr"$\gamma = {i:.0f}/7$"
+            label_text = fr"$\gamma = {i:.0f}\,/\,7$"
 
-        plt.plot(tmp[:,1], tmp[:,2], marker="o", markersize = 6, markeredgewidth = 1.0, mec = "black", label = label_text)
+        plt.plot(tmp[:,1], tmp[:,2], marker="o", markersize = 6, markeredgewidth = 1.0, lw=2.5, mec = "black", label = label_text)
+        # gradient_line.gradient_line(ax=plt.gca(), x=tmp[:,1], y=tmp[:,2], c=tmp[:,0])
 
     plt.ylabel(r"$\Delta E~[\mathrm{meV}]$")
     plt.xlabel(r"$r_0$")
 
     plt.legend()
-    plt.savefig("e_barrier.png")
+    plt.savefig("e_barrier_vs_r0.png", bbox_inches="tight", dpi=300)
+
+    plt.close()
+    r0_list= np.unique(data[:,1])
+    # Plot vs gamma
+    for r0 in r0_list:
+        tmp = data[ data[:,1] == r0 ]
+        tmp = tmp[ np.argsort(tmp[:,0]) ]
+
+        label_text = fr"$r_0 = {r0} a$"
+
+        plt.plot(tmp[:,0], tmp[:,2], marker="o", markersize = 6, markeredgewidth = 1.0, lw=2.5, mec = "black", label = label_text)
+
+    plt.ylabel(r"$\Delta E~[\mathrm{meV}]$")
+    plt.xlabel(r"$\gamma$")
+    plt.gca().set_xticks( [i/7.0 for i in range(8)] )
+
+    xtick_labels = [ f"{i}/7" for i in range(8)]
+    xtick_labels[0]  = "0"
+    xtick_labels[-1] = "1"
+
+    plt.gca().set_xticks( [ i/7.0 for i in range(8)] )
+    plt.gca().set_xticklabels( xtick_labels )
+
+    plt.legend()
+    plt.savefig("e_barrier_vs_gamma.png", bbox_inches="tight", dpi=300)
+
 
 if __name__ == "__main__":
     import calculation_folder
