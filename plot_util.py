aca84e2dc4d3c66387f8e4e2066ae779f8ac0900
Moritz <m.sallermann@fz-juelich.de>
2022-05-18 16:29:08 +0200

    plot_util: annotate params can now specify font largeness and added clip_cube function

--- plot_util.py
+++ plot_util.py
@@ -1,4 +1,4 @@
-def annotate_params(path_to_png, gamma, r0, dpi=300):
+def annotate_params(path_to_png, gamma, r0, dpi=300, fontsize=15):
     import matplotlib.pyplot as plt
     dpi = 300
     img = plt.imread(path_to_png)
@@ -6,7 +6,7 @@ def annotate_params(path_to_png, gamma, r0, dpi=300):
     figsize = width / float(dpi), height / float(dpi)
     fig = plt.figure(figsize=figsize)
     ax = fig.add_axes([0, 0, 1, 1])
-    plt.text(0, 1, rf"$\gamma = {gamma:.2f}$  $r_0 = {r0:.2f}\,a$", fontsize = 18, bbox = dict(facecolor='white', edgecolor="white", alpha=0.80), horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
+    plt.text(0, 1, rf"$\gamma = {gamma:.2f}$  $r_0 = {r0:.2f}\,a$", fontsize = fontsize, bbox = dict(facecolor='white', edgecolor="white", alpha=0.80), horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
     ax.axis('off')
     ax.imshow(img)
     fig.savefig(path_to_png, dpi=300, bbox_inches=0, pad_inches = 0)
@@ -42,7 +42,7 @@ def get_pyvista_plotter(chain_file, n_cells, idx_image_infile, DELAUNAY_PATH="de
         plotter.camera_focal_point = center
         plotter.camera_up          = np.cross(normal, [1,0,0])
 
-    return plotter
+    return plotter, center, normal
 
 def add_preimages(plotter, N=16, sz=0.2):
     from spirit_extras import import_spirit, post_processing, plotting
@@ -52,3 +52,25 @@ def add_preimages(plotter, N=16, sz=0.2):
         spin = [np.sin(phi),np.cos(phi),sz]
         colors = plotting.get_rgba_colors( [spin] )
         plotter.add_preimage(spin, tol=0.15, n_neighbours=24, render_args={"color" : colors[0][:3]})
+
+def clip_cube(center, normal, cube_type, infinity=100, zero=1):
+    import numpy as np
+    import pyvista as pv
+
+    axis  = np.cross(normal, [0,0,1])
+    angle = -np.arccos(normal[2]) * 180/np.pi
+
+    if cube_type.lower() == "normal":
+        bounds = (center[0], center[0]+infinity, center[1]-infinity, center[1]+infinity, center[2]-infinity, center[2]+infinity)
+    elif cube_type.lower() == "plane":
+        bounds = (center[0]-infinity, center[0]+infinity, center[1]-infinity, center[1]+infinity, center[2], center[2]+infinity)
+    elif cube_type.lower() == "slice_normal":
+        bounds = (center[0]-zero, center[0]+zero, center[1]-infinity, center[1]+infinity, center[2]-infinity, center[2]+infinity)
+    elif cube_type.lower() == "slice_plane":
+        bounds = (center[0]-infinity, center[0]+infinity, center[1]-infinity, center[1]+infinity, center[2]-zero, center[2]+zero)
+    else:
+        raise Exception("Invalid type")
+
+    clip_cube = pv.Cube(bounds=bounds).rotate_vector( vector = axis, angle = angle, point=center, inplace=True )
+
+    return clip_cube
