9f60fac5372355038f5d9e18a436073143769503
Moritz <m.sallermann@fz-juelich.de>
2022-10-20 16:38:11 +0200

    ema.py: some changes

--- ema.py
+++ ema.py
@@ -1,10 +1,40 @@
-import os
+import os, sys
+import calculation_folder
 import sys
-from typing import no_type_check
-import compute_abc
 
-def main():
-    ### Import Spirit modules
+from spirit_extras import import_spirit, data
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import calculation_folder
+
+SCRIPT_DIR = os.path.dirname( os.path.abspath(__file__) )
+INPUT_FILE = SCRIPT_DIR + "/input.cfg"
+
+quiet = False
+
+def main(path):
+    from spirit import simulation
+    calculation = calculation_folder.calculation_folder(path)
+    output_name = "gneb_sp3"
+
+    # print(path)
+    # print("gamma = {}, l0 = {}".format(calculation.descriptor["gamma"], calculation.descriptor["l0"]))
+    # if os.path.exists( calculation.to_abspath(output_name) ):
+    #     print("Skip")
+    #     return
+    # print("---")
+
+    calculation.lock()
+
+    def state_prepare_cb(p_state):
+        from spirit import geometry, configuration, hamiltonian
+        geometry.set_n_cells(p_state, calculation.descriptor["n_cells"])
+        configuration.domain(p_state, [0,0,1])
+        hamiltonian.set_exchange(p_state, len(calculation.descriptor["J"]), calculation.descriptor["J"])
+
+    initial_chain_file = calculation.to_abspath(os.path.join(output_name , "chain.ovf"))
+
     from spirit import state
     from spirit import configuration
     from spirit import simulation
@@ -15,34 +45,28 @@ def main():
     import time
     import numpy as np
 
-    cfgfile = "input.cfg"
-    quiet = False
+    N_MODES = 7
+    with state.State(INPUT_FILE, quiet) as p_state:
+        state_prepare_cb(p_state)
+        io.chain_read(p_state, initial_chain_file)
+        epath = data.energy_path_from_p_state(p_state)
+        idx   = 1
 
-    CHAIN_FILE = "/home/moritz/hopfion_simulations/output_bogo_30_unpinned_sc_ci_shrinking/chain.ovf"
+        parameters.ema.set_n_modes(p_state, N_MODES, idx_image=idx)
+        parameters.ema.set_sparse(p_state, True, idx_image=idx)
+        print("start")
+        simulation.start(p_state, simulation.METHOD_EMA, n_iterations=1, idx_image = idx)
+        io.eigenmodes_write(p_state, calculation.to_abspath("eigenmodes_sp.ovf"), idx_image=idx)
 
-    N_MODES = 20
-
-    for K in [0]:
-        with state.State(cfgfile, quiet) as p_state:
-            io.chain_read(p_state, CHAIN_FILE)
-
-            for idx in [8]:
-                # hamiltonian.set_anisotropy(p_state, K, [0,0,1], idx_image=0)
-                # simulation.start(p_state, simulation.METHOD_LLG, simulation.SOLVER_LBFGS_OSO, idx_image=0)
-
-                parameters.ema.set_n_modes(p_state, N_MODES, idx_image=idx)
-                parameters.ema.set_sparse(p_state, True, idx_image=idx)
-                simulation.start(p_state, simulation.METHOD_EMA, n_iterations=1, idx_image = idx)
-
-                io.eigenmodes_write(p_state, "eigenmodes_sp_{}.ovf".format(K), idx_image=idx)
 
 if __name__ == "__main__":
-    from spirit_python_utilities.spirit_extras import import_spirit, gneb_workflow, data, plotting
 
-    os.environ["OMP_NUM_THREADS"] = "16"
+    spirit_info = import_spirit.find_and_insert("~/Coding/spirit_hopfion", stop_on_first_viable=True )[0]
 
-    def choose_spirit(x):
-        return "afeb6181bd4f1".startswith(x.revision) and x.openMP and x.pinning # check for solver info revision and openMP and pinning
-    spirit_info = import_spirit.find_and_insert("~/Coding", stop_on_first_viable=True, choose = choose_spirit )[0]
+    import argparse, os
+    parser = argparse.ArgumentParser()
+    parser.add_argument("paths", type=str, nargs="+")
+    args = parser.parse_args()
 
-    main()
+    for f in args.paths:
+        main(f)
